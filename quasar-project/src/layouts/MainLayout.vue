d435a55ee382b291c7fef7f37d69e3fe23f2b490
François Mounous <Francois Mounous>
2024-01-27 07:53:13 +0100

    [SEXIER] : footer tabscroller

--- quasar-project/src/layouts/MainLayout.vue
+++ quasar-project/src/layouts/MainLayout.vue
@@ -1,96 +1,27 @@
 <template>
-  <div class="q-pa-md bg-image">
-    <q-layout view="lHh Lpr lFf">
-      <q-header elevated>
-        <q-toolbar>
-          <q-btn
-            flat
-            dense
-            round
-            icon="menu"
-            aria-label="Menu"
-            @click="leftDrawerOpen = !leftDrawerOpen"
-          />
-
-          <q-toolbar-title> CreditPlay </q-toolbar-title>
-
-          <div>FREE beta version 0.0.1</div>
-        </q-toolbar>
-      </q-header>
-
-      <q-drawer
-        v-model="leftDrawerOpen"
-        show-if-above
-        :width="200"
-        :breakpoint="400"
+    <q-layout view="hhh lpr fFf">
+      <q-footer class="gutter">
+        <q-tabs
+        v-model="tab"
+        class="bg-primary text-white shadow-2 gutter"
+        :outside-arrows="false"
+        :mobile-arrows="false"
       >
-        <q-scroll-area
-          style="
-            height: calc(100% - 150px);
-            margin-top: 150px;
-            border-right: 1px solid #152570;
-          "
-        >
-          <q-list padding>
-            <q-item to="/" exact clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="feed" />
-              </q-item-section>
+        <q-route-tab name="start" icon="feed" to="/" />
+        <q-route-tab name="events" icon="toc" to="/events" :disable="initFormDone==false"/>
+        <q-route-tab name="summary" icon="money" to="/summary" :disable="initFormDone==false"/>
+        <q-route-tab name="chart" icon="bar_chart" to="/summary" :disable="initFormDone==false"/>
+        <q-route-tab name="help" icon="help" to="/help"/>
 
-              <q-item-section> Situation initiale </q-item-section>
-            </q-item>
-
-            <q-item to="/events" exact clickable v-ripple :disable="initFormDone==false">
-              <q-item-section avatar>
-                <q-icon name="toc" />
-              </q-item-section>
-
-              <q-item-section> Evenements </q-item-section>
-            </q-item>
-
-            <q-item to="/summary" exact clickable v-ripple  :disable="initFormDone==false">
-              <q-item-section avatar>
-                <q-icon name="bar_chart" />
-              </q-item-section>
-
-              <q-item-section> Résumé </q-item-section>
-            </q-item>
-
-            <q-item to="/help" exact clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="help" />
-              </q-item-section>
-
-              <q-item-section> aide </q-item-section>
-            </q-item>
-          </q-list>
-        </q-scroll-area>
-
-        <q-img
-          class="absolute-top"
-          src="https://upload.wikimedia.org/wikipedia/commons/1/10/Tursiops_truncatus_01.jpg"
-          style="height: 150px"
-          fit="fill"
-        >
-          <div class="absolute-bottom bg-transparent">
-            <q-avatar size="56px" class="q-mb-sm">
-              <img
-                src="https://expernet-campus.re/wp-content/uploads/2021/03/cliches-developpeur-informatique.png"
-              />
-            </q-avatar>
-            <div class="text-weight-bold">Francois Mounous</div>
-            <div>@bottineEnNubuck</div>
-          </div>
-        </q-img>
-      </q-drawer>
-
+      </q-tabs>
+      </q-footer>
       <q-page-container>
         <KeepAlive>
           <router-view />
         </KeepAlive>
       </q-page-container>
     </q-layout>
-  </div>
+
 </template>
 
 <script setup lang="ts">
@@ -99,7 +30,7 @@
 import { SessionStorage } from 'quasar';
 import { ref,watch } from 'vue';
 import {startFormFilled} from 'stores/example-store'
-var leftDrawerOpen = ref(false);
+var tab=ref('start');
 var  initFormDone =ref(false);
 const setInitDone=function(b_in:boolean){
   initFormDone.value=b_in};
