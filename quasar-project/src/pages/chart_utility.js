09a316977bca4d61481ef576f1cbbf8fd02d7e06
François Mounous <Francois Mounous>
2024-03-31 18:36:14 +0200

    enhance graph cat display

--- quasar-project/src/pages/chart_utility.js
+++ quasar-project/src/pages/chart_utility.js
@@ -1,7 +1,16 @@
 const getFormatedCategories=function(categories,numberofPointsTodisplay){
   var categoriesFmt= categories;
+  var step=0;
   //display 10 points : compute step
-  const step=categoriesFmt.length/numberofPointsTodisplay;
+  if(categoriesFmt.length>numberofPointsTodisplay)
+  {
+    step=Number(categoriesFmt.length/numberofPointsTodisplay);
+  }
+  else
+  {
+    step=1;
+  }
+
   for(var i=0;i<categoriesFmt.length;i++)
   {
     if(i%step!=0 && i!=(categoriesFmt.length-1))
