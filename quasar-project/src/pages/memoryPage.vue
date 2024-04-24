7454d8223a14cf8256727591a32c5b4cc7fd9256
François Mounous <Francois Mounous>
2024-04-24 07:23:48 +0200

    Memory page : popup

--- quasar-project/src/pages/memoryPage.vue
+++ quasar-project/src/pages/memoryPage.vue
@@ -1,84 +1,110 @@
 <template>
-  <QPage>
-    <q-list class="bg-primary" separator bordered>
-      <q-item v-for="item in listSave" :key="item.id" clickable @click="selected_id==item.id ? selected_id=DEFAULT_ID:selected_id = item.id" v-ripple>
-        <q-item-section>
-          <q-item-label>{{ item.name }}</q-item-label>
-        </q-item-section>
-      </q-item>
-    </q-list>
-    <q-btn label="tout supprimer" @click="deleteAllData"></q-btn>
-    <q-btn label="Sauvegarder la simulation en cours" @click="saveCurrentData"></q-btn>
-    <q-btn label="Supprimer la simulation" @click="deleteData()" :disable="selected_id==DEFAULT_ID"></q-btn>
-    <q-btn label="Restaurer la simulation" @click="restoreData()" :disable="selected_id==DEFAULT_ID"></q-btn>
-  </QPage>
+  <q-page>
+    <div>
+    <div>
+      <q-list class="bg-primary" separator bordered>
+        <q-item v-for="item in listSave" :key="item.id" clickable
+          @click="selected_id == item.id ? selected_id = DEFAULT_ID : selected_id = item.id" v-ripple>
+          <q-item-section>
+            <q-item-label>{{ item.name }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+    <div class="col flex flex center q-ma-md">
+      <div>
+        <q-btn class="q-ma-md" label="Tout supprimer" color="blue-grey-8" @click="deleteAllData"></q-btn>
+      </div>
+      <div>
+        <q-btn class="q-ma-md" label="Sauvegarder la simulation en cours" color="blue-grey-8" @click="mustPopName = true"></q-btn>
+      </div>
+      <div>
+        <q-btn class="q-ma-md" label="Supprimer la simulation" color="blue-grey-8" @click="deleteData"
+        :disable="selected_id == DEFAULT_ID"></q-btn>
+      </div>
+      <div>
+        <q-btn class="q-ma-md" label="Restaurer la simulation" color="blue-grey-8" @click="restoreData"
+        :disable="selected_id == DEFAULT_ID"></q-btn>
+      </div>
+    </div>
+    <div class="col q-ma-md">
+      <q-popup-proxy v-model="mustPopName" cover transition-show="scale" transition-hide="scale">
+        <div class="q-ma-xl">
+          Donner un nom à la sauvegarde ?
+        </div>
+        <q-input class="q-ma-md" v-model="currentName"></q-input>
+        <q-btn class="q-ma-md" label="Nom par Défaut" @click="saveCurrentData"></q-btn>
+        <q-btn class="q-ma-md" label="Ajouter un nom " @click="saveCurrentData"></q-btn>
+      </q-popup-proxy>
+
+    </div>
+  </div>
+  </q-page>
+
 
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
 import { LocalStorage } from 'quasar';
 import { onBeforeMount } from 'vue';
-import { bank,simu,startFormFilled } from 'src/stores/store';
+import { bank, simu, startFormFilled } from 'src/stores/store';
 import { useRouter } from 'vue-router';
-const DEFAULT_ID=-1;
-const DEFAULT_NAME='';
+const DEFAULT_ID = -1;
+const DEFAULT_NAME = '';
+var mustPopName = ref(false);
 var listSave = ref([]);
 var selected_id = ref(DEFAULT_ID);
-var currentName=ref(DEFAULT_NAME);
+var currentName = ref(DEFAULT_NAME);
 const router = useRouter();
-const getStorage = function () {
-  if (LocalStorage.has('listSave')) {
-    listSave.value = LocalStorage.getItem('listSave');
-  }
+
+const saveCurrentData = function () {
+  mustPopName.value = false
+  listSave.value.push({ simu: simu.value, bank: bank.value, id: listSave.value.length, name: currentName.value == DEFAULT_NAME ? 'Simulation ' + String(listSave.value.length) : currentName.value, startFormFilled: startFormFilled.value });
+  LocalStorage.set('listSave', listSave.value);
+  currentName.value=DEFAULT_NAME;
 }
-const saveCurrentData=function(){
-  listSave.value.push({simu : simu.value,bank:bank.value,id:listSave.value.length,name:currentName.value==DEFAULT_NAME ? 'Simulation '+String(listSave.value.length) : currentName.value,startFormFilled:startFormFilled.value});
-  LocalStorage.set('listSave',listSave.value);
-}
-const getSimuIndexInStorage=function(id_in)
-{
-  var index=-1;
+const getSimuIndexInStorage = function (id_in) {
+  var index = -1;
   //get the index in the list of simu
-  for(var i=0;i<listSave.value.length;i++)
-  {
-    if((listSave.value)[i].id==id_in)
-    {
-      index=i;
+  for (var i = 0; i < listSave.value.length; i++) {
+    if ((listSave.value)[i].id == id_in) {
+      index = i;
       break;
     }
   }
   return index;
 }
-const restoreData=function(){
-  var index=getSimuIndexInStorage(selected_id.value);
-  if(index==-1)
-  {
+const getStorage = function () {
+  if (LocalStorage.has('listSave')) {
+    listSave.value = LocalStorage.getItem('listSave');
+  }
+}
+const restoreData = function () {
+  var index = getSimuIndexInStorage(selected_id.value);
+  if (index == -1) {
     //notify
   }
-  else
-  {
-    simu.value=(listSave.value)[index].simu;
-    bank.value=(listSave.value)[index].bank;
-    startFormFilled.value=(listSave.value)[index].startFormFilled;
+  else {
+    simu.value = (listSave.value)[index].simu;
+    bank.value = (listSave.value)[index].bank;
+    startFormFilled.value = (listSave.value)[index].startFormFilled;
     router.push('/lineChart');
   }
 }
-const deleteData=function()
-{
-  var index=getSimuIndexInStorage(selected_id.value);
-  if(index==-1)
-  {
+const deleteData = function () {
+  var index = getSimuIndexInStorage(selected_id.value);
+  if (index == -1) {
     //notify
   }
-  else
-  {
-    listSave.value.splice(i,1);
+  else {
+    listSave.value.splice(index, 1);
+    LocalStorage.set('listSave', listSave.value);
   }
 }
-const deleteAllData=function(){
-  listSave.value=[];
-  LocalStorage.set('listSave',listSave.value);
+const deleteAllData = function () {
+  listSave.value = [];
+  LocalStorage.set('listSave', listSave.value);
 }
 onBeforeMount(getStorage);
 </script>
