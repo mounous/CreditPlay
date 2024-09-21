db2787fea5d554f824e768c932cceff211dfc248
François Mounous <Francois Mounous>
2024-09-21 07:04:05 +0200

    OUTRO for INTRO

--- quasar-project/src/components/AppIntro.vue
+++ quasar-project/src/components/AppIntro.vue
@@ -5,11 +5,7 @@
         <q-carousel-slide v-for="i in slides" :key=Number(i) :name=i class="column no-wrap flex-center">
           <div style="display: flex;flex-direction: column;align-items: center;align-content: center;justify-content: space-around;justify-items: center;width: 100%;height:100%">
             <AppTeasing v-if="i==1" @tuto-teasing-finished="slide='4'"></AppTeasing>
-            <div class="q-ma-md text-center" style="display: flex;flex-direction: column;align-items: center;" v-if="i==10">
-              <th class="q-mt-md q-ml-md q-mr-md q-mb-xs" style="color: white;font-size:25px;">{{transStr(stringsIDs.str_tuto_icon)}}</th>
-              <q-icon name="help" size="xl" class="q-ma-xl"></q-icon>
-              <q-btn @click="emit('tuto-intro-finished')" label="OK" color="blue-grey-8"></q-btn>
-            </div>
+            <AppOutro v-if="i==10" @tuto-outro-finished="emit('tuto-intro-finished')" ></AppOutro>
             <AppKnowledge v-if="i==5" @tuto-know-finished="slide='6'"></AppKnowledge>
             <AppExtra v-if="i==6" @tuto-extra-finished="slide='9'"></AppExtra>
             <AppCapabilities v-if="i==4" ></AppCapabilities>
@@ -21,6 +17,7 @@
 </template>
 
 <script setup>
+import AppOutro from './AppOutro.vue';
 import { ref,defineEmits } from 'vue'
 import {stringsIDs,transStr} from '../stores/languages'
 import ProcessExplainer from './ProcessExplainer.vue';
---
+++ quasar-project/src/components/AppOutro.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="q-ma-md text-center" style="display: flex;flex-direction: column;align-items: center;" >
+      <th class="q-mt-md q-ml-md q-mr-md q-mb-xs" style="color: white;font-size:25px;">{{transStr(stringsIDs.str_tuto_icon)}}</th>
+      <q-icon name="help" size="xl" class="rotating q-ma-xl"></q-icon>
+      <shakeBtn @click="emit('tuto-outro-finished')"  btn-label='OK'></shakeBtn>
+    </div>
+</template>
+
+<script setup>
+import shakeBtn from './shakeBtn.vue';
+import {defineEmits } from 'vue'
+import {stringsIDs,transStr} from '../stores/languages'
+
+const emit=defineEmits(['tuto-outro-finished']);
+
+</script>
+
+<style scoped lang="css">
+
+@-webkit-keyframes rotating /* Safari and Chrome */ {
+  from {
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  to {
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes rotating {
+  from {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  to {
+    -ms-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+.rotating {
+  -webkit-animation: rotating 1.5s linear ;
+  -moz-animation: rotating 1.5s linear ;
+  -ms-animation: rotating 1.5s linear ;
+  -o-animation: rotating 1.5s linear ;
+  animation: rotating 1.5s linear ;
+}
+
+</style>
+
